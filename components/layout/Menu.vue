37fbb4c5e317831382f4b70192e14f2f2210cf8b
unknown <ssakoks@mail.ru>
2024-04-29 17:54:42 +0500

    build: add nuxt icon

--- components/layout/Menu.vue
+++ components/layout/Menu.vue
@@ -1,9 +1,26 @@
-<script setup lang='ts'>
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>Избранное</div>
-  <div>Корзина</div>
-  <div>Профиль</div>
+	<div>
+		<Icon
+			size="22"
+			name="ic:baseline-favorite-border"
+			color="black"
+		/>
+		<span>Избранное</span>
+	</div>
+	<div>
+		<Icon
+			size="22"
+			name="ic:outline-shopping-cart"
+			color="black"
+		/><span>Корзина</span>
+	</div>
+	<div>
+		<Icon
+			size="22"
+			name="ic:outline-account-circle"
+			color="black"
+		/><span>Профиль</span>
+	</div>
 </template>
